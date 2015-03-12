0618761b7f502c7b16541fb6282365b569062f25
boran.zeng <boran.zeng@shenma-inc.com>
2015-03-12 20:43:26 +0800

    rename events and support interruption of animation

---
+++ src/global.js
@@ -0,0 +1,106 @@
+/**
+ * Created by Administrator on 2015/3/12.
+ */
+/**
+ * Created by 柏然 on 2014/12/12.
+ */
+function RenderGlobal(opt) {
+  if(!(this instanceof RenderGlobal))return new RenderGlobal(opt);
+  opt=opt||{};
+  this._tasks = {};
+  this._defaultTaskName=opt.defaultTaskName||'default';
+  this._invalid=true;
+  this._persistStyles={};
+  this._persistElement=document.createElement('style');
+  this._styleElement=document.createElement('style');
+}
+Flip.RenderGlobal = RenderGlobal;
+RenderGlobal.EVENT_NAMES = {
+  FRAME_START: 'frameStart',
+  FRAME_END: 'frameEnd',
+  UPDATE: 'update'
+};
+inherit(RenderGlobal, Flip.util.Object, {
+  get defaultTask(){
+    var taskName=this._defaultTaskName,t=this._tasks[taskName];
+    if(!t)this.add(t=new RenderTask(taskName));
+    return t;
+  },
+  getTask:function(name,createIfNot){
+    if(!name)return this.defaultTask;
+    var r=this._tasks[name];
+    if(!r&&createIfNot) {
+      r=new RenderTask(name);
+      this.add(r)
+    }
+    return r;
+  },
+  add: function (obj) {
+    var task, taskName, tasks;
+    if (obj instanceof RenderTask) {
+      if (!(taskName = obj.name))
+        throw Error('task must has a name');
+      else if ((tasks=this._tasks).hasOwnProperty(taskName))
+        throw Error('contains same name task');
+      else if (tasks[taskName]=obj) {
+        obj._global=this;
+        obj.timeline.start();
+        return this.invalid();
+      }
+    }
+    else if (obj instanceof Animation || obj instanceof Clock)
+      return this.defaultTask.add(obj);
+    return false;
+  },
+  immediate:function(style){
+    var styles=this._persistStyles,uid=nextUid('immediateStyle'),self=this,cancel;
+    styles[uid]=style;
+    cancel=function cancelImmediate(){
+      var style=styles[uid];
+      delete styles[uid];
+      self._persistStyle=false;
+      return style;
+    };
+    cancel.id=uid;
+    this._persistStyle=false;
+    return cancel;
+  },
+  refresh:function(){
+    this._foreceRender=true;
+  },
+  init: function () {
+    if(typeof window==="object"){
+      var head=document.head,self=this;
+      if(!this._styleElement.parentNode){
+        head.appendChild(this._styleElement);
+        head.appendChild(this._persistElement);
+      }
+      Flip.fallback(window);
+      window.addEventListener('resize',function(){self.refresh()});
+      this.loop();
+    }
+    this.init = function () {
+      console.warn('The settings have been initiated,do not init twice');
+    };
+  },
+  invalid:function(){
+    return this._invalid=true;
+  },
+  loop: function (element) {
+    loopGlobal(this);
+    window.requestAnimationFrame(this.loop.bind(this), element||window.document.body);
+  },
+  apply:function(){
+    if(!this._persistStyle){
+      var styles=[];
+      objForEach(this._persistStyles,function(style){styles.push(style);});
+      this._persistElement.innerHTML=styles.join('\n');
+      this._persistStyle=true;
+    }
+  },
+  createRenderState: function () {
+    return {global: this, task:null,styleStack:[],forceRender:this._foreceRender}
+  }
+});
+FlipScope.global = new RenderGlobal();
+
