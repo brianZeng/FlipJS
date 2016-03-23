d82b04765d7efb3929c30ea70cb43a7474a568e5
brian <zbr80259@alibaba-inc.com>
2016-03-23 10:58:09 +0800

    Add GLRender Files

---
+++ src/webgl/base/GLSamplerCube.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2015/4/3.
+ */
+function GLSamplerCube(opt){
+  if (!(this instanceof GLSamplerCube)) {
+    return new GLSamplerCube(opt);
+  }
+}
---
+++ src/webgl/base/GLTexture.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Administrator on 2015/4/1.
+ */
+function GLTexture(isCube){
+  this._index = -1;
+  this._type = isCube ? WebGLRenderingContext.TEXTURE_CUBE_MAP : WebGLRenderingContext.TEXTURE_2D;
+}
+GLTexture.prototype = {
+  dispose: function (gl){
+    var handle = this._glHandle;
+    if (handle) {
+      gl.deleteTexture(handle);
+    }
+  },
+  getGlHandle: function (gl){
+    return this._glHandle || (this._glHandle = gl.createTexture())
+  },
+  finalize: function (gl, resMng){
+    resMng.releaseTextureIndex(this._index);
+    var handle = this._glHandle;
+    if (handle) {
+      gl.deleteTexture(handle);
+    }
+  },
+  bind: function (gl){
+    var mng = gl.resMng, index = this._index;
+    if (index == -1) {
+      this._index = index = mng.increaseTextureIndex();
+    }
+    gl.activeTexture(WebGLRenderingContext['TEXTURE' + index]);
+    gl.bindTexture(this._type, this.getGlHandle(gl))
+  }
+};
